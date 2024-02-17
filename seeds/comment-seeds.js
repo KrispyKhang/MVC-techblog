b4f0b974e95ef0d2162745eeea8763d74ce6dff8
Khang Tran <creamybanana@Khang.invcorp.invensense.com>
2024-02-16 21:20:50 -0800

    worked on the handlebars with tutor and chat a while ago

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -14,12 +14,12 @@ const commentData = [
   {
     user_id: 1,
     post_id: 4,
-    comment_text: "Awesome! kudos to everyone who have contributed",
+    comment_text: "Wow, amazing work!",
   },
   {
     user_id: 3,
     post_id: 5,
-    comment_text: "We just reached a million subscribers! Fantastic!",
+    comment_text: "Daaaang, this is cool!",
   },
   {
     user_id: 3,
